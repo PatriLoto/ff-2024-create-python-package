808d0478f0370a95a0c29b72854e20289739387b
Patricia A. Loto <39300655+PatriLoto@users.noreply.github.com>
2024-10-30 16:15:12 +0000

    add: new module to the package

---
+++ src/myfirstpackage/add_number.py
@@ -0,0 +1,16 @@
+"""A module that adds numbers together"""
+
+def add_num(number1, number2):
+    """ A function that adds two numbers together
+
+    Args:
+        number1 : int
+            The first number that the function will add.
+        number2 : int
+            The second number that the function will add.
+
+        Return
+        ------
+            The sum of two numbers
+    """
+    return number1 + number2
